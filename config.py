0d882455d6d1086f18da610e31d414fc0d08089f
ddosvps9953 <ddosravi316@gmail.com>
2025-02-17 09:02:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY RAVI-X-DDOS
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8048715452:AAGTCGd4NhblkQ6QmULlL4G96fL9MDLe40Y")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@R_SDanger")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7129010361")
+
+
+
+
+
+
+
+  
